48998f4e276dc7d6f6076c4e486bcf9f8ffbf4c1
isaias1127 <isaias.tellez1127@gmail.com>
2017-04-27 20:52:03 -0500

    5subida

--- js/acciones.js
+++ js/acciones.js
@@ -25,6 +25,6 @@ $(document).ready(function (e){
 });//document
 
 function escribehistoria(accion){
-	$('#eHistoria').append('<li>'+accion+'<li>');
+	$('#eHistoria').append('<li>'+accion+'</li>');
 }
 	
